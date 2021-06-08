add0d3078d7afd0a18c46b6ed6f7f341b7d6e6d5
Warlord-dev <lingdar0719@gmail.com>
2021-06-08 19:35:50 +0300

    update

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -9,9 +9,9 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 205 26" {...props}>
-      <image width="205" height="26" href={isDark ? '/images/egg/LogoTextNewDark.png' : '/images/egg/LogoTextNewWhite.png'}/>
-    </Svg>
+    // <Svg viewBox="0 0 205 26" {...props}>
+      <image width="205" height="26" href={isDark ? '/images/LogoDark.png' : '/images/LogoWhite.png'}/>
+    // </Svg>
   )
 };
 
